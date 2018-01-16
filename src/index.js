2ce0c0b364295702b1cddff388abab9510435bb9
YanPanMichael <yanpancsu@icloud.com>
2018-01-17 01:35:31 +0800

    toggle test

--- src/index.js
+++ src/index.js
@@ -4,50 +4,46 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-
-class Clock extends React.Component {
+class Toggle extends React.Component {
     constructor(props) {
-        super(props);
-        this.state = {
-            date: new Date()
-        };
+      super(props);
+      this.state = {isToggleOn: true};
+  
+      // This binding is necessary to make `this` work in the callback
+      this.handleClick = 
+        this.handleClick.bind(this);
+    }
+  
+    handleClick() {
+      this.setState(prevState => ({
+        isToggleOn: !prevState.isToggleOn
+      }));
     }
 
-    componentDidMount() {
-        this.timerID = setInterval(() => this.tick(),1000);        
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.timerID);
-    }
-
+    // This syntax ensures `this` is bound within handleClick.
+    // Warning: this is *experimental* syntax.
+    // handleClick = () => {
+    //   this.setState(prevState => ({
+    //     isToggleOn: !prevState.isToggleOn
+    //   }));
+    // }
+  
     render() {
-        return (
-            <div>
-                <h1>Hello, world!</h1>
-                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-            </div>
-        );
-    }
-
-    tick() {
-        this.setState({
-            date: new Date()
-        });
+      return (
+        <button onClick={this.handleClick}>
+          {this.state.isToggleOn ? 'ON' : 'OFF'}
+        </button>
+      );
+        // // This syntax ensures `this` is bound within handleClick
+        // return (
+        //     <button onClick={(e) => this.handleClick(e)}>
+        //     Click me
+        //     </button>
+        // );
     }
 }
-
+  
 ReactDOM.render(
-    <Clock />,
+    <Toggle />,
     document.getElementById('root')
-)
-
-// 1) 当 <Clock /> 被传递给 ReactDOM.render() 时，React 调用 Clock 组件的构造函数。 由于 Clock 需要显示当前时间，所以使用包含当前时间的对象来初始化 this.state 。 我们稍后会更新此状态。
-
-// 2) React 然后调用 Clock 组件的 render() 方法。这是 React 了解屏幕上应该显示什么内容，然后 React 更新 DOM 以匹配 Clock 的渲染输出。
-
-// 3) 当 Clock 的输出插入到 DOM 中时，React 调用 componentDidMount() 生命周期钩子。 在其中，Clock 组件要求浏览器设置一个定时器，每秒钟调用一次 tick()。
-
-// 4) 浏览器每秒钟调用 tick() 方法。 在其中，Clock 组件通过使用包含当前时间的对象调用 setState() 来调度UI更新。 通过调用 setState() ，React 知道状态已经改变，并再次调用 render() 方法来确定屏幕上应当显示什么。 这一次，render() 方法中的 this.state.date 将不同，所以渲染输出将包含更新的时间，并相应地更新DOM。
-
-// 5) 一旦Clock组件被从DOM中移除，React会调用componentWillUnmount()这个钩子函数，定时器也就会被清除
+);
