5d24d7d67ca9358f2878fae44b965f8fb4924220
YanPanMichael <yanpancsu@icloud.com>
2018-01-17 00:28:11 +0800

    version3

--- src/index.js
+++ src/index.js
@@ -4,23 +4,57 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-function Welcome(props) {
-    return <h1>hello, {props.name}</h1>
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <UserInfo author={props.author} />
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
 }
 
-function Elements() {
+function UserInfo(props) {
+    const author = props.author;
     return (
-        <div>
-            <Welcome name="Haha" />
-            <Welcome name="Cande" />
-            <Welcome name="Edite" />
+        <div className="UserInfo">
+            <Avatar user={author} />
+            <div className="UserInfo-name">
+                {author.name}
+            </div>
         </div>
+    );
+}
+
+function Avatar(props) {
+    return (
+        <img className="Avatar"
+            src={props.user.avatarUrl}
+            alt={props.user.name}
+        />
     )
 }
 
-ReactDOM.render(
-    <Elements />,
+function formatDate(date) {
+    return date.toLocaleDateString();
+}
+
+const comment = {
+    date: new Date(),
+    text: 'I hope you enjoy learning React!',
+    author: {
+      name: 'Hello Kitty',
+      avatarUrl: 'http://placekitten.com/g/64/64'
+    }
+  };
+  ReactDOM.render(
+    <Comment
+      date={comment.date}
+      text={comment.text}
+      author={comment.author} />,
     document.getElementById('root')
 );
-
-// registerServiceWorker();
