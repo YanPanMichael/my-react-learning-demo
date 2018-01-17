a8b244206cb8365e3e29709de7ec2cde6e922c7b
YanPanMichael <yanpancsu@icloud.com>
2018-01-18 01:14:50 +0800

    define key value in loop

--- src/index.js
+++ src/index.js
@@ -43,7 +43,74 @@ class Toggle extends React.Component {
     }
 }
   
+class Page extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {showWarning: true}
+    this.handleToggleClick = this.handleToggleClick.bind(this);
+  }
+
+  handleToggleClick() {
+    this.setState(prevState => ({
+      showWarning: !prevState.showWarning
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <WarningBanner warn={this.state.showWarning} />
+        <button onClick={this.handleToggleClick}>
+          {this.state.showWarning ? 'Hide' : 'Show'}
+        </button>
+      </div>
+    )
+  }
+}
+
+function WarningBanner(props) {
+  if (!props.warn) {
+    return null;
+  }
+
+  return (
+    <div className="warning">
+      Warning!
+    </div>
+  );
+}
+
+function ListItem(props) {
+  // Correct! There is no need to specify the key here:
+  return <li>{props.value}</li>;
+}
+
+function NumberList(props) {
+  const numbers = props.numbers;
+  const listItems1 = numbers.map((number) =>
+    <li key={number.toString()}>
+      {number}
+    </li>
+  );
+  const listItems2 = numbers.map((number, index) =>
+    <li key={index}>
+      {number}
+    </li>
+  );
+  const listItems3 = numbers.map((number, index) => 
+    <ListItem key={number.toString()} value={number} />
+  );
+  return (
+    <div>
+      <ul>{listItems1}</ul>
+      <ul>{listItems2}</ul>
+      <ul>{listItems3}</ul>
+    </div>
+  );
+}
+
+const numbers = [1, 2, 3, 4, 5];
 ReactDOM.render(
-    <Toggle />,
-    document.getElementById('root')
+  <NumberList numbers={numbers} />,
+  document.getElementById('root')
 );
